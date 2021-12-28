391c48dc0b3e7d5335dd3ad2c76572b996347ba6
krishpranav <krisna.pranav@gmail.com>
2021-12-28 11:02:12 +0530

    lib: board item

--- lib/board_item.dart
+++ lib/board_item.dart
@@ -36,3 +36,98 @@ class BoardItem extends StatefulWidget {
     return BoardItemState();
   }
 }
+
+class BoardItemState extends State<BoardItem> with AutomaticKeepAliveClientMixin{
+  late double height;
+  double? width;
+
+  @override
+  bool get wantKeepAlive => true;
+
+  void onDropItem(int? listIndex, int? itemIndex) {
+    if (widget.onDropItem != null) {
+      widget.onDropItem!(listIndex, itemIndex,widget.boardList!.widget.boardView!.startListIndex,widget.boardList!.widget.boardView!.startItemIndex, this);
+    }
+    widget.boardList!.widget.boardView!.draggedItemIndex = null;
+    widget.boardList!.widget.boardView!.draggedListIndex = null;
+    if(widget.boardList!.widget.boardView!.listStates[listIndex!].mounted) {
+      widget.boardList!.widget.boardView!.listStates[listIndex].setState(() { });
+    }
+  }
+
+  void _startDrag(Widget item, BuildContext context) {
+    if (widget.boardList!.widget.boardView != null) {
+      widget.boardList!.widget.boardView!.onDropItem = onDropItem;
+      if(widget.boardList!.mounted) {
+        widget.boardList!.setState(() { });
+      }
+      widget.boardList!.widget.boardView!.draggedItemIndex = widget.index;
+      widget.boardList!.widget.boardView!.height = context.size!.height;
+      widget.boardList!.widget.boardView!.draggedListIndex =
+          widget.boardList!.widget.index;
+      widget.boardList!.widget.boardView!.startListIndex =
+          widget.boardList!.widget.index;
+      widget.boardList!.widget.boardView!.startItemIndex = widget.index;
+      widget.boardList!.widget.boardView!.draggedItem = item;
+      if (widget.onStartDragItem != null) {
+        widget.onStartDragItem!(
+            widget.boardList!.widget.index, widget.index, this);
+      }
+      widget.boardList!.widget.boardView!.run();
+      if(widget.boardList!.widget.boardView!.mounted) {
+        widget.boardList!.widget.boardView!.setState(() { });
+      }
+    }
+  }
+
+  void afterFirstLayout(BuildContext context) {
+    try {
+      height = context.size!.height;
+      width = context.size!.width;
+    }catch(e){}
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    WidgetsBinding.instance!
+        .addPostFrameCallback((_) => afterFirstLayout(context));
+    if (widget.boardList!.itemStates.length > widget.index!) {
+      widget.boardList!.itemStates.removeAt(widget.index!);
+    }
+    widget.boardList!.itemStates.insert(widget.index!, this);
+    return GestureDetector(
+      onTapDown: (otd) {
+        if(widget.draggable) {
+          RenderBox object = context.findRenderObject() as RenderBox;
+          Offset pos = object.localToGlobal(Offset.zero);
+          RenderBox box = widget.boardList!.context.findRenderObject() as RenderBox;
+          Offset listPos = box.localToGlobal(Offset.zero);
+          widget.boardList!.widget.boardView!.leftListX = listPos.dx;
+          widget.boardList!.widget.boardView!.topListY = listPos.dy;
+          widget.boardList!.widget.boardView!.topItemY = pos.dy;
+          widget.boardList!.widget.boardView!.bottomItemY =
+              pos.dy + object.size.height;
+          widget.boardList!.widget.boardView!.bottomListY =
+              listPos.dy + box.size.height;
+          widget.boardList!.widget.boardView!.rightListX =
+              listPos.dx + box.size.width;
+
+          widget.boardList!.widget.boardView!.initialX = pos.dx;
+          widget.boardList!.widget.boardView!.initialY = pos.dy;
+        }
+      },
+      onTapCancel: () {},
+      onTap: () {
+        if (widget.onTapItem != null) {
+          widget.onTapItem!(widget.boardList!.widget.index, widget.index, this);
+        }
+      },
+      onLongPress: () {
+        if(!widget.boardList!.widget.boardView!.widget.isSelecting && widget.draggable) {
+          _startDrag(widget, context);
+        }
+      },
+      child: widget.item,
+    );
+  }
+}
